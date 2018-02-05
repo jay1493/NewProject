44a27f91efbca1e188e948d487e49df65fa7f728
jay1493 <anubhav_singh1493@hotmail.com>
2018-02-05 19:32:41 +0530

    New Project

---
+++ app/src/main/java/com/anubhav_singh/infoedgeassignment/database/ResturantsDatabase.java
@@ -0,0 +1,10 @@
+package com.anubhav_singh.infoedgeassignment.database;
+
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by Anubhav-Singh on 05-02-2018.
+ */
+
+public abstract class ResturantsDatabase extends RoomDatabase {
+}
